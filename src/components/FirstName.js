d6803c450d4bace99ecf93e5070f4316a91bca31
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-18 11:02:28 +0400

    const name = useSelector((state) => state.user)

--- src/components/FirstName.js
+++ src/components/FirstName.js
@@ -2,6 +2,7 @@ import React from 'react'
 import{useSelector} from 'react-redux';
 
 const FirstName = () => {
+    const name = useSelector((state) => state.user)
     return <div className='font-bold'>First Name</div>
 }
 
