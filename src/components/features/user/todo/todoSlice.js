b27041847a4986916af3eb28ffec97be02e8e571
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-19 14:48:16 +0400

    form .js import { useDispatch } from 'react-redux'

--- src/components/features/user/todo/todoSlice.js
+++ src/components/features/user/todo/todoSlice.js
@@ -1,1 +1,26 @@
 // создаю список задач в features создала папку todo в ней todoSelector.js
+
+import { createSlice } from "@reduxjs/toolkit";
+
+
+//создаю стейт для этого апликейшена
+const initialState={
+    todos:[],
+}
+
+//создаю сам slice:
+
+export const todoSlice = createSlice ({
+    name: "todo",
+    initialState,
+    reducers:{
+        //функция которая принимает (state, action)
+        addTodo:(state, action)=> {
+state.todos.push(action.payload)
+        },
+
+}, //тут {state.todos.push(action.payload)}  будем чтото вводить нажимать sambit и будет добавляться         addTodo:(state, action)=>{stat
+})
+
+export const {addTodo} = todoSlice.actions // из addTodo вытянули todoSlice.actions 
+export default todoSlice.reducer//  и теперь его подключаем возвращаемся stor  и точно также 
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../components/features/user/userSlice";
+import todoSlice from "../components/features/user/todo/todoSlice";
 
-export const store =configureStore({
-    reducer:{
-        user:userSlice,// поле, ключ user значение userSlice
-    },
-})//слайс отвечает за ту или иную часть приложения
-
+export const store = configureStore({
+  reducer: {
+    user: userSlice, // поле, ключ user значение userSlice
+    todo: todoSlice,
+  },
+}); //слайс отвечает за ту или иную часть приложения
 
 //создали тут глобальный обьект стора где храниться весь наш стейт пото создаем для каждой сущности отдельный useSlise смотреть в нём
