bc42c73205f1525297cc31af64f345b904862805
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-19 14:23:29 +0400

    в lastname const lastName = useSelector((state) => state.user.lastName)

--- src/components/LastName.js
+++ src/components/LastName.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const LastName = () => {
+    const lastName = useSelector((state) => state.user.lastName)
+    
     return <div className='font-bold'>Last Name</div>
 }
 
