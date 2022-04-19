168c2830e3c533d603af134187b94ab0a2cded67
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-04-19 23:57:04 +0400

    post: todoSlice,// подключила

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../components/features/user/userSlice";
 import todoSlice from "../components/features/user/todo/todoSlice";
+import postSlice from "../components/features/user/post/postSlice";
 
 export const store = configureStore({
   reducer: {
     user: userSlice, // поле, ключ user значение userSlice
     todo: todoSlice,
+    post: todoSlice,// подключила
   },
 }); //слайс отвечает за ту или иную часть приложения
 
